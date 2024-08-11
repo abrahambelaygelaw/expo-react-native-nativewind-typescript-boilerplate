f8234d82d28f8424407bffc006ba4bf5ef3657ba
Abraham Belay <abrahambelaygelaw@gmail.com>
2024-08-11 16:18:17 +0300

    set up

---
+++ app/(onboarding)/onboarging-three.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const OnboardingThree = () => {
+  return (
+    <View>
+      <Text>OnboardingThree</Text>
+    </View>
+  );
+};
+
+export default OnboardingThree;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -54,7 +54,7 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
     </ThemeProvider>
